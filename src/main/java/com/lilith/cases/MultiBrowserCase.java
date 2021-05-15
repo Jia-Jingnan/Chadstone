f2140031793598accef3b333f66249aef8da5cea
Jia-Jingnan <875480307@qq.com>
2021-05-16 03:11:58 +0800

    0516-完善判断方法，根据输入的浏览器名称选择使用哪一种浏览器执行自动化测试

--- src/main/java/com/lilith/cases/MultiBrowserCase.java
+++ src/main/java/com/lilith/cases/MultiBrowserCase.java
@@ -1,5 +1,9 @@
 package com.lilith.cases;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
@@ -12,6 +16,23 @@ public class MultiBrowserCase {
     @Parameters(value = {"browser"})
     @Test
     public void test(String browser){
+
+        WebDriver driver = null;
+
         System.out.println("浏览器：" + browser);
+        if ("firefox".equalsIgnoreCase(browser)){
+            System.setProperty("webdriver.gecko.driver", "src/main/java/com/lilith/driver/geckodriver");
+            driver = new FirefoxDriver();
+        } else if ("chrome".equalsIgnoreCase(browser)){
+            System.setProperty("webdriver.chrome.driver", "src/main/java/com/lilith/driver/chromedriver");
+            driver = new ChromeDriver();
+        } else {
+            System.out.println("暂不支持该浏览器类型");
+            return;
+        }
+        driver.get("http://127.0.0.1/youtest/html/login.html");
+        driver.findElement(By.name("username")).sendKeys("002@qq.com");
+        driver.findElement(By.name("password")).sendKeys("123123");
+        driver.findElement(By.id("login")).click();
     }
 }
