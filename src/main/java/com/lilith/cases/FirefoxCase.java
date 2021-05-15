c1a7937ddf88d2f70832169c2ad39453b9069103
Jia-Jingnan <875480307@qq.com>
2021-05-16 02:44:50 +0800

    0516-添加FirefoxCase，执行操作，打开youtest/login.html页面，输入用户名密码，点击登陆

---
+++ src/main/java/com/lilith/cases/FirefoxCase.java
@@ -0,0 +1,20 @@
+package com.lilith.cases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * @Author:JiaJingnan
+ * @Date: 上午2:40 2021/5/16
+ */
+public class FirefoxCase {
+
+    public static void main(String[] args) {
+        WebDriver driver = new FirefoxDriver();
+        driver.get("http://127.0.0.1/youtest/html/login.html");
+        driver.findElement(By.name("username")).sendKeys("002@qq.com");
+        driver.findElement(By.name("password")).sendKeys("123123");
+        driver.findElement(By.id("login")).click();
+    }
+}
