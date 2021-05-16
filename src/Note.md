df1043015c54dd4b6c437992f22293094801dcd8
Jia-Jingnan <875480307@qq.com>
2021-05-17 00:30:08 +0800

    0516-非Xpath元素定位

--- src/Note.md
+++ src/Note.md
@@ -8,4 +8,9 @@
 在没有id时考虑name  
 3.根据元素的样式来定位元素，可能返回的是一个元素集合，页面含有多个相同样式的元素，
 多个元素使用findElements定位元素，从集合中取出要用的元素，通过索引取出元素，单个使用findElement定位元素  
-4.tagName:根据元素的标签名来定位元素，取元素的用法同className
+4.tagName:根据元素的标签名来定位元素，取元素的用法同className  
+5.linkText:根据超链接文本值定位元素,partialLinkText 
+6.cssSelector: css选择器定位元素
+    id:id选择器
+    样式选择器定位
+    标签选择器
--- src/main/java/com/lilith/cases/ElementCase.java
+++ src/main/java/com/lilith/cases/ElementCase.java
@@ -19,6 +19,16 @@ public class ElementCase extends BaseCase {
         driver.navigate().to("http://localhost/youtest/html/login.html");
         driver.findElement(By.name("username")).sendKeys("002@qq.com");
 
+        // 根据超文本连接定位元素
+        driver.get("http://www.baidu.com");
+        driver.findElement(By.linkText("新闻")).click();
+
+        driver.findElement(By.partialLinkText("图")).click();
+
+        // 通过样式选择器定位
+        driver.findElement(By.cssSelector("#username")).sendKeys("002@qq.com");
+
+
         Thread.sleep(1000);
 
         driver.quit();
