270740127d33c101b47713293796a2fd90f7e460
Jia-Jingnan <875480307@qq.com>
2021-05-16 17:28:50 +0800

    0516-抽取出BaseCase类，将浏览器driver初始化及浏览器的选择放入类中，加入@BeforeSuite注解，在测试套件执行之前执行

--- src/main/java/com/lilith/cases/BaseCase.java
+++ src/main/java/com/lilith/cases/BaseCase.java
@@ -1,25 +1,24 @@
 package com.lilith.cases;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 /**
  * @Author:JiaJingnan
- * @Date: 上午2:58 2021/5/16
+ * @Date: 下午5:24 2021/5/16
  */
-public class MultiBrowserCase {
+public class BaseCase {
+
+    public static WebDriver driver;
 
     @Parameters(value = {"browser"})
-    @Test
-    public void test(String browser) throws Exception{
-
-        WebDriver driver = null;
+    @BeforeSuite
+    public void init(String browser) throws Exception{
 
         System.out.println("浏览器：" + browser);
         if ("firefox".equalsIgnoreCase(browser)){
@@ -32,18 +31,5 @@ public class MultiBrowserCase {
             System.out.println("暂不支持该浏览器类型");
             return;
         }
-
-
-        driver.findElement(By.name("username")).sendKeys("002@qq.com");
-        driver.findElement(By.name("password")).sendKeys("123123");
-        driver.findElement(By.id("login")).click();
-
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        // 关闭浏览器
-        driver.quit();
     }
 }
