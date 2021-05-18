da546ba2cd056061efbac535e0e30e2474237a25
Jia-Jingnan <875480307@qq.com>
2021-05-18 10:00:49 +0800

    0518-163邮箱登陆页面iframe定位，先通过xpaht定位到iframe，然后切换到iframe，最后通过普通定位方式找到输入框，进行操作

---
+++ src/main/java/com/lilith/cases/IFrameCase.java
@@ -0,0 +1,23 @@
+package com.lilith.cases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+/**
+ * @Author:JiaJingnan
+ * @Date: 上午9:42 2021/5/18
+ */
+public class IFrameCase extends BaseCase{
+
+    @Test
+    public void test(){
+        driver.get("https://mail.163.com/");
+        WebElement iframe = driver.findElement(By.xpath("//div/iframe"));
+        System.out.println(iframe.getTagName());
+        driver.switchTo().frame(iframe);
+        driver.findElement(By.name("email")).sendKeys("Knowit123@163.com");
+
+
+    }
+}
