a63464961b1a75c18a5e1c41c357a75facdb93e8
Jia-Jingnan <875480307@qq.com>
2021-05-19 13:12:36 +0800

    0519-增加注册数据源Excel表对应的实体类

---
+++ src/main/java/com/lilith/entity/Register.java
@@ -0,0 +1,19 @@
+package com.lilith.entity;
+
+import lombok.Data;
+
+/**
+ * @Author:JiaJingnan
+ * @Date: 下午1:00 2021/5/19
+ * 注册数据实体类
+ */
+@Data
+public class Register {
+
+    private String isPostive;
+    private String desc;
+    private String username;
+    private String password;
+    private String passwordConfirm;
+    private String expectedTips
+}
