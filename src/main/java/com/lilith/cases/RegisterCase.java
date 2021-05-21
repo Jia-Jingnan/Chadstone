fffbdf1e8e4cc6506a93b48f3f919ba5fbdc127a
Jia-Jingnan <875480307@qq.com>
2021-05-21 12:21:53 +0800

    0521-完成反向用例的测试

--- src/main/java/com/lilith/cases/RegisterCase.java
+++ src/main/java/com/lilith/cases/RegisterCase.java
@@ -2,6 +2,7 @@ package com.lilith.cases;
 
 import com.lilith.util.RegisterUtil;
 import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
@@ -13,20 +14,21 @@ import org.testng.annotations.Test;
 public class RegisterCase extends BaseCase {
 
     @Test(dataProvider = "datas")
-    public void test(String username, String password, String pwdconfirm, String exceptedtips){
+    public void testNegative(String username, String password, String pwdconfirm, String expectedTips){
         // 访问注册页面
         driver.get("http://localhost/youtest/html/register.html");
         // 用户名
-        driver.findElement(By.id("username")).sendKeys();
+        driver.findElement(By.id("username")).sendKeys(username);
         // 密码
-        driver.findElement(By.id("password")).sendKeys();
+        driver.findElement(By.id("password")).sendKeys(password);
         // 确认密码
-        driver.findElement(By.id("pwdconfirm")).sendKeys();
+        driver.findElement(By.id("pwdconfirm")).sendKeys(pwdconfirm);
         // 立即注册按钮
         driver.findElement(By.className("submit_btn")).click();
 
         // tips
-        driver.findElement(By.className("tips")).getText();
+        String actualTips = driver.findElement(By.className("tips")).getText();
+        Assert.assertEquals(actualTips,expectedTips);
     }
 
 
@@ -36,4 +38,5 @@ public class RegisterCase extends BaseCase {
         Object[][] datas = RegisterUtil.getNegativeDatas(cellNames);
         return datas;
     }
+
 }
