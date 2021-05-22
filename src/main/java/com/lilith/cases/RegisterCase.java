2aae4d2e0bb233c64eb4b16ba8f360c9ddcf39b0
Jia-Jingnan <875480307@qq.com>
2021-05-22 16:40:16 +0800

    0522-增加一个测试正向用例的test方法，并增加一个取正向测试数据的dataProvider，通过判断跳转的url是否为登陆页面来断言成功或失败

--- src/main/java/com/lilith/cases/RegisterCase.java
+++ src/main/java/com/lilith/cases/RegisterCase.java
@@ -13,7 +13,7 @@ import org.testng.annotations.Test;
  */
 public class RegisterCase extends BaseCase {
 
-    @Test(dataProvider = "datas")
+    @Test(dataProvider = "negativeDatas")
     public void testNegative(String username, String password, String pwdconfirm, String expectedTips){
         // 访问注册页面
         driver.get("http://localhost/youtest/html/register.html");
@@ -31,11 +31,37 @@ public class RegisterCase extends BaseCase {
         Assert.assertEquals(actualTips,expectedTips);
     }
 
+    @Test(dataProvider = "positiveDatas")
+    public void testPositive(String username, String password, String pwdconfirm){
+        // 访问注册页面
+        driver.get("http://localhost/youtest/html/register.html");
+        // 用户名
+        driver.findElement(By.id("username")).sendKeys(username);
+        // 密码
+        driver.findElement(By.id("password")).sendKeys(password);
+        // 确认密码
+        driver.findElement(By.id("pwdconfirm")).sendKeys(pwdconfirm);
+        // 立即注册按钮
+        driver.findElement(By.className("submit_btn")).click();
+
+        // 获取地址，是否跳转到登陆页面
+        String currentUrl = driver.getCurrentUrl();
+        boolean contains = currentUrl.contains("login.html");
+        Assert.assertTrue(contains);
+    }
+
 
     @DataProvider
-    public Object[][] datas(){
+    public Object[][]  negativeDatas(){
         String[] cellNames = {"Username", "Password", "PasswordConfirm", "ExpectedTips"};
-        Object[][] datas = RegisterUtil.getNegativeDatas(cellNames);
+        Object[][] datas = RegisterUtil.getDatas("0",cellNames);
+        return datas;
+    }
+
+    @DataProvider
+    public Object[][]  positiveDatas(){
+        String[] cellNames = {"Username", "Password", "PasswordConfirm", "ExpectedTips"};
+        Object[][] datas = RegisterUtil.getDatas("1", cellNames);
         return datas;
     }
 
