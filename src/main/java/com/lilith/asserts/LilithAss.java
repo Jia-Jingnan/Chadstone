b8b3651095c466f46f298c64411a11beb1313e04
Jia-Jingnan <875480307@qq.com>
2021-05-24 01:46:31 +0800

    0524-断言类中新增方法，传入element和text，会比较传入元素的文本值与传入的text是否包含，包含返回true，否则返回false

--- src/main/java/com/lilith/asserts/LilithAss.java
+++ src/main/java/com/lilith/asserts/LilithAss.java
@@ -1,6 +1,7 @@
 package com.lilith.asserts;
 
 import com.lilith.cases.BaseCase;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -12,16 +13,29 @@ import org.testng.Assert;
  */
 public class LilithAss {
 
+    public static WebDriverWait wait = new WebDriverWait(BaseCase.driver,30);
+
     public static void assertUrlContains(String urlContains){
 
-        WebDriverWait wait = new WebDriverWait(BaseCase.driver,30);
-        boolean isContainPage = true;
+        boolean isContainUrl = true;
         try {
             wait.until(ExpectedConditions.urlContains(urlContains));
         } catch (Exception e){
-            isContainPage = false;
+            isContainUrl = false;
         }
-        Assert.assertTrue(isContainPage);
+        Assert.assertTrue(isContainUrl);
+    }
+
+    public static void assertTextPresent(WebElement element, String text){
+        boolean textToBePresentElement = true;
+        try {
+            textToBePresentElement = wait.until(ExpectedConditions.textToBePresentInElement(element, text));
+        } catch (Exception e){
+            textToBePresentElement = false;
+        }
+        Assert.assertTrue(textToBePresentElement);
+
+
     }
 
 
